4427711fadc47c2eacf090a011295171174723aa
maksim kireev <maksimkir987@gmail.com>
2023-11-06 10:57:50 +0300

    commit message task 2

---
+++ task002/Program.cs
@@ -0,0 +1,17 @@
+﻿double b1, k1, b2, k2;
+
+Console.Write("Введите значение b1: ");
+b1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение k1: ");
+k1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение b2: ");
+b2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение k2: ");
+k2 = double.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения: ({x}; {y})");
