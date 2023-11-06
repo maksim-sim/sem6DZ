d9574cd264c08b180c3420af18d2a76ffba31e17
maksim kireev <maksimkir987@gmail.com>
2023-11-06 10:57:04 +0300

    commit mes task001

---
+++ task001/Program.cs
@@ -0,0 +1,15 @@
+﻿int EnterMes(string Message){
+    Console.Write(Message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int count = 0;
+int countNamber = EnterMes("Введите колличество введённых чисел: ");
+for(int i = 1; i <= countNamber; i++){
+    int namber = EnterMes($"Введите число {i}: ");
+    if(namber > 0) count++;
+}
+
+Console.WriteLine($"Количество чисел больше нуля: {count}");
